9d2bf4055d2bea4fd08a564bc3ab76886a13c513
Heaust Azure <54670936+Heaust-ops@users.noreply.github.com>
2025-05-06 18:53:21 +0530

    added a shitty ecs that I'm confident is way worse than just looping … (#4)
    * added a shitty ecs that I'm confident is way worse than just looping objects lol
    
    * mader a rendering system based on ecs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import "./style.css";
 import { attachCanvas, connectWorker } from "../core/connection/mainThread.ts";
 import MainSceneWorker from "../examples/starterScene?worker";
+import { ECS } from "../core/ecs/ecs";
 
 const mainSceneWorker = new MainSceneWorker();
 connectWorker(mainSceneWorker);
 attachCanvas(mainSceneWorker, "rendering-canvas");
+
+(window as any).ecs = new ECS();
